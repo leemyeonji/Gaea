f981ce509a51bdf2d204d5fa855aa90206771d5d
leemyeonji <61834038+leemyeonji@users.noreply.github.com>
2021-06-15 19:22:45 +0900

    Card transition

--- Gaea_SwiftUIWork/Gaea_SwiftUIWork/HomeView.swift
+++ Gaea_SwiftUIWork/Gaea_SwiftUIWork/HomeView.swift
@@ -8,42 +8,64 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State var show = false
+    
     var body: some View {
-        ScrollView(.vertical) {
-            ZStack {
-                VStack {
-
-                    Text("Gaea")
-                        .font(.system(size:  34, weight: .heavy, design: .monospaced))
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding(.leading, 27)
-                        .padding(.top, 12)
-                        .padding(.bottom, 12)
-                    
-                    Rectangle()
-                        .frame(height: 1)
-                        .opacity(0.2)
-                    
-                    
-                    Text("Olympus")
-                        .font(.system(size: 20, weight: .bold, design: .monospaced))
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding(.leading, 27)
-                        .padding(.top, 22)
-                    
-                    ScrollView(.horizontal, showsIndicators: false) {
-                        HStack(spacing: 30) {
-                            Card(goddess: goddess[0])
-                                .frame(width: 300, height: 420)
+        if show == false {
+            ScrollView(.vertical) {
+                ZStack {
+                    VStack {
+                        Text("Gaea")
+                            .font(.system(size:  34, weight: .heavy, design: .monospaced))
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .padding(.leading, 27)
+                            .padding(.top, 12)
+                            .padding(.bottom, 12)
+                        
+                        Rectangle()
+                            .frame(height: 1)
+                            .opacity(0.2)
+                        
+                        
+                        Text("Olympus")
+                            .font(.system(size: 20, weight: .bold, design: .monospaced))
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .padding(.leading, 27)
+                            .padding(.top, 22)
+                        
+                        
+                        
+                        ScrollView(.horizontal, showsIndicators: false) {
+                            HStack(spacing: 30) {
+                                Card(goddess: goddess[0])
+                                    .frame(width: 300, height: 420)
+                                    .onTapGesture {
+                                        show.toggle()
+                                    }
+                            }
+                            .padding(.leading, 27)
+                            .padding(.bottom, 60)
+                            
                         }
-                        .padding(.leading, 27)
-                        .padding(.bottom, 60)
-                        
-                   
-                        
                     }
-                    
-                    
+                }
+            }
+        } else {
+            ZStack {
+                CardDetail(goddess: goddess[0])
+                
+                HStack {
+                    Spacer()
+                    VStack {
+                        Image(systemName: "xmark.octagon.fill")
+                            .font(.system(size: 36))
+                            .opacity(0.8)
+                            .offset(x: -16)
+                            .onTapGesture {
+                                show = false
+                            }
+                        Spacer()
+                    }
                 }
             }
         }
