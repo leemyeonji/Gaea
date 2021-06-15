457002fcef87c7a8cb03046cdf56585d3519cb94
leemyeonji <61834038+leemyeonji@users.noreply.github.com>
2021-06-15 17:23:17 +0900

    Card Detail View

--- Gaea_SwiftUIWork/Gaea_SwiftUIWork/CardDetail.swift
+++ Gaea_SwiftUIWork/Gaea_SwiftUIWork/CardDetail.swift
@@ -9,31 +9,63 @@ import SwiftUI
 
 struct CardDetail: View {
     var body: some View {
-        VStack {
-            VStack {
-                Spacer()
-                
-                VStack(alignment: .leading) {
-                    Text("Demeter")
-                        .font(.custom("Dida", size: 44))
-                        .foregroundColor(Color("NameYellow"))
-                    Text("Olympian goddess of the harvest and agriculture, presiding over grains and the fertility of the earth.")
-                        .font(.system(size: 13, weight: .light, design: .monospaced))
+        
+        ScrollView {
+            VStack(alignment: .center) {
+                ZStack {
+                    Image("Ceres_Hendrick Goltzius")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                    
+                    
+                    VStack(alignment: .leading) {
+                        Spacer()
+                        Text("Demeter")
+                            .font(.custom("Dida", size: 60))
+                            .foregroundColor(Color("NameYellow"))
+                            .shadow(color: .black.opacity(0.3), radius: 4, x: 0, y: 10)
+                    }
+                    .padding(.all, 9)
+                    .padding(.bottom, 9)
+                    .frame(maxWidth: .infinity)
                 }
-                .padding(.all, 9)
-                .background(VisualEffectBlur(blurStyle: .systemUltraThinMaterial))
+                .frame(maxWidth: .infinity, maxHeight: 620)
+                .clipShape(RoundedRectangle(cornerRadius: 0, style: .continuous))
+                .shadow(color: .black.opacity(0.25), radius: 20, x: 0.0, y: 10)
+                .edgesIgnoringSafeArea(.all)
+                
+                
+                HStack(spacing: 14) {
+                    Image(systemName: "bookmark")
+                        .font(.system(size: 24))
+                    Image(systemName: "square.and.arrow.up")
+                        .font(.system(size: 24))
+                    Text("Olympian goddess of the harvest and agriculture, presiding over grains and the fertility of the earth.")
+                        .font(.system(size: 14, weight: .bold, design: .monospaced))
+                        .multilineTextAlignment(.trailing)
+                }
+                .padding(.top, 9)
+                .padding(.horizontal, 9)
+                
+                Divider()
+                
+                VStack(spacing: 30) {
+                    Text("In ancient Greek religion and mythology, Demeter is the Olympian goddess of the harvest and agriculture, presiding over grains and the fertility of the earth. She was also called Deo. Her cult titles include Sito, 'she of the Grain', as the giver of food or grain, and Thesmophoros, 'giver of customs' and/or 'legislator', in association with the secret female-only festival called the Thesmophoria.")
+                    
+                    Text("Though Demeter is often described simply as the goddess of the harvest, she presided also over the sacred law, and the cycle of life and death. She and her daughter Persephone were the central figures of the Eleusinian Mysteries, a religious tradition that predated the Olympian pantheon, and which may have its roots in the Mycenaean period c. 1400–1200 BC.")
+                    
+                    Text("Demeter was often considered to be the same figure as the Anatolian goddess Cybele, and she was identified with the Roman goddess Ceres.")
+                        
+                }
+                .font(.system(size: 16, weight: .light, design: .monospaced))
+                .lineSpacing(8.0)
+                .multilineTextAlignment(.leading)
+                .padding(.all, 32)
             }
-            .background(
-                Image("Ceres_Hendrick Goltzius")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-            )
-            .clipShape(RoundedRectangle(cornerRadius: 0, style: .continuous))
-            .shadow(color: .black.opacity(0.25), radius: 20, x: 0.0, y: 10)
             
-            
-            Spacer()
         }
+        
+        .ignoresSafeArea()
     }
 }
 
